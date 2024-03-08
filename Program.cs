2f14a47a23a35d6068837fda01c69b5e9a0acc9c
Elvin Ismayilov <0utsawg@gmail.com>
2024-03-08 23:38:23 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿namespace _5Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            // ----------------------------------- Daxil edilən ədədin bölənlərini çıxarın. ------------------------------------------------------
+
+
+            //Console.WriteLine("Eded daxil edin bolenlerini gorun");
+            //int num = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    if (num % i == 0)
+            //    { 
+            //    Console.Write(i + " ");
+            //    }
+            //}
+
+
+            // ------------------------------------ Verilmiş array-da ən kiçik elementi tapın. ---------------------------------------------
+
+
+            //int[] arr = { 1, 2, 3, 4, 5 };
+            //int num = Int32.MaxValue;
+
+            //foreach (int i in arr)
+            //{
+
+            //    if (num > i)
+            //    {
+            //        num = i;
+            //    }
+
+            //}
+            //Console.WriteLine(num);
+
+
+
+            // ----------------------- Verilmiş string-in içərisindəki a hərflərinin b ilə əvəz edin. ----------------------------------------------
+
+
+            //string text = "Salam";
+            //string text2 = text.Replace('a', 'b');
+            //Console.WriteLine(text2); 
+
+
+
+
+            // -------------------------------------- Daxil edilən ədədin tərsini yazın.Məs, 157 - 751. ----------------------------------------------
+
+
+            //Console.WriteLine("Eded daxil edin");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+
+            //int result = 0;
+            //while (num > 0)
+            //{
+            //    result = num % 10;
+            //    num = num / 10; 
+            //    Console.Write(result);
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
